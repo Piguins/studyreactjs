a279944a6a0304568272f562e42c4d068110a120
kiethongngu <21522263@gm.uit.edu.vn>
2023-09-18 10:27:44 +0700

    storage job

--- studyreactjs/src/App.js
+++ studyreactjs/src/App.js
@@ -5,14 +5,22 @@ function App () {
   const [jobs, setJobs] = useState([])
   
   const handleSubmit = () => {
-    setJobs(prev  => [...prev, job])
-    setJob = ('')  
+    setJobs (prev => {
+      const newJobs = [...prev, job]
+
+      const jsonJobs = JSON.stringify(newJobs)
+
+      localStorage.setItem('jobs', jsonJobs)
+
+      return newJobs
+    })
+    setJob ('')
   }
   return (
     <div>
       <input value = {job}
       onChange = {e => setJob(e.target.value)}/>
-      <button onClick={handleSubmit}> Add </button>
+      <button onClick = {handleSubmit}> Add </button>
 
       <ul>
         {jobs.map((job, index) => (
